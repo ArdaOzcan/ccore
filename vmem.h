bc10f0fc9978d8842bea81844211484976823aaa
Arda Özcan <ardaozcan05@gmail.com>
2025-09-22 00:05:42 +0300

    Made CCore ISO C90

--- vmem.h
+++ vmem.h
@@ -3,21 +3,37 @@
 #ifdef _WIN32
 #include <windows.h>
 #else
-#include <unistd.h>
 #include <sys/mman.h>
+#include <unistd.h>
+#if defined(MADV_DONTNEED)
+#elif defined(__linux__)
+#define MADV_DONTNEED 4
+#elif defined(__FreeBSD__) || defined(__OpenBSD__)
+#define MADV_DONTNEED 4
+#elif defined(__APPLE__)
+#define MADV_DONTNEED 4
+#endif
 #endif
 
-static void* vmem_reserve(size_t size) {
+static void*
+vmem_reserve(size_t size)
+{
 #ifdef _WIN32
     return VirtualAlloc(NULL, size, MEM_RESERVE, PAGE_NOACCESS);
 #else
-    void* ptr = mmap(NULL, size, PROT_NONE,
-                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
+    void* ptr = mmap(NULL,
+                     size,
+                     PROT_NONE,
+                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE,
+                     -1,
+                     0);
     return ptr == MAP_FAILED ? NULL : ptr;
 #endif
 }
 
-static int vmem_commit(void* ptr, size_t size) {
+static int
+vmem_commit(void* ptr, size_t size)
+{
 #ifdef _WIN32
     return VirtualAlloc(ptr, size, MEM_COMMIT, PAGE_READWRITE) != NULL;
 #else
@@ -25,19 +41,29 @@ static int vmem_commit(void* ptr, size_t size) {
 #endif
 }
 
-// static int vmem_decommit(void* ptr, size_t size) {
-// #ifdef _WIN32
-//     return VirtualFree(ptr, size, MEM_DECOMMIT) != 0;
-// #else
-//     return madvise(ptr, size, MADV_DONTNEED) == 0;
-// #endif
-// }
+static int
+vmem_decommit(void* ptr, size_t size)
+{
+#ifdef _WIN32
+    return VirtualFree(ptr, size, MEM_DECOMMIT) != 0;
+#else
+#if defined(MADV_DONTNEED)
+    return madvise(ptr, size, MADV_DONTNEED) == 0;
+#else
+    /* Fallback: do nothing */
+    (void)ptr;
+    (void)size;
+    return 0;
+#endif
+#endif
+}
 
-static void vmem_release(void* ptr, size_t size) {
+static void
+vmem_release(void* ptr, size_t size)
+{
 #ifdef _WIN32
     VirtualFree(ptr, 0, MEM_RELEASE);
 #else
     munmap(ptr, size);
 #endif
 }
-
